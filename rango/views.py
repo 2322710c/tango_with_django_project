f8fcd7739368fc8fb5c38559e054bce2ac41c53a
“Yi-Chu <2322710c@student.gla.ac.uk>
2018-01-17 19:18:02 +0000

    add about page and link

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>.")
+
+def about(request):
+    return HttpResponse("<a href='/rango/'>Index</a>")
 
 
 
