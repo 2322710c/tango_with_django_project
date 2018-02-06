6466a50b35474eb7520b10762bfe76f5534d5928
“Yi-Chu <2322710c@student.gla.ac.uk>
2018-02-06 14:06:47 +0000

    bug fix

--- rango/forms.py
+++ rango/forms.py
@@ -4,7 +4,7 @@ from rango.models import Page, Category , UserProfile
 
 class CategoryForm(forms.ModelForm):
     name= forms.CharField(max_length=128,
-                          help_text="Please enter the category name.")
+                          help_text="Please enter the category name." )
     views =forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes =forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -14,6 +14,7 @@ class CategoryForm(forms.ModelForm):
         # Provide an association between the ModelForm and a model
         model = Category
         fields =('name',)
+       
 
 class PageForm(forms.ModelForm):
     title=forms.CharField(max_length=128,
